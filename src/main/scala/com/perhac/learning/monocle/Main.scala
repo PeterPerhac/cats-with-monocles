9d89fb8de853a2a8019356a13a78f851860e1ac2
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-03-25 15:17:33 +0000

    some cats for the monocles

--- src/main/scala/com/perhac/learning/monocle/Main.scala
+++ src/main/scala/com/perhac/learning/monocle/Main.scala
@@ -1,5 +1,10 @@
 package com.perhac.learning.monocle
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.language.postfixOps
+
 case class Street(number: Int, name: String)
 
 case class Address(city: String, street: Street)
@@ -10,6 +15,9 @@ case class Employee(name: String, company: Company)
 
 object Main {
 
+  import cats.instances.future._
+  import cats.instances.list._
+  import cats.syntax.traverse._
   import monocle.Lens
   import monocle.macros.GenLens
 
@@ -18,17 +26,19 @@ object Main {
   val street: Lens[Address, Street] = GenLens[Address](_.street)
   val streetName: Lens[Street, String] = GenLens[Street](_.name)
 
-
   val capitaliseEachWordInString: String => String = _.split(' ').map(_.capitalize).mkString(" ")
+  val fixStreetName = company composeLens address composeLens street composeLens streetName modify capitaliseEachWordInString
+  val capitaliseStreetName = (e: Employee) => Future(fixStreetName(e))
 
   def main(args: Array[String]): Unit = {
-    val employee1 = Employee("john", Company("awesome inc", Address("london", Street(23, "high street"))))
-    val employee2 = Employee("paul", Company("awesome inc", Address("london", Street(23, "low avenue"))))
+    val employees = List(
+      Employee("john", Company("awesome inc", Address("london", Street(1, "high street")))),
+      Employee("paul", Company("awesome inc", Address("london", Street(2, "low avenue")))),
+      Employee("tony", Company("awesome inc", Address("london", Street(3, "some kind of place")))),
+      Employee("pete", Company("awesome inc", Address("london", Street(4, "shady lane"))))
+    )
 
-    val fixStreetName = company composeLens address composeLens street composeLens streetName modify capitaliseEachWordInString
-
-    List(employee1, employee2).map(fixStreetName).foreach(println)
-
+    Await.result(employees.traverse(capitaliseStreetName).map(_.foreach(println)), 1 second)
   }
 
 }
